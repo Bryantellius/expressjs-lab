4f1a6d30d6469ec42bb51c66f926f16c2a5cc9d0
Ben Bryant <brbryant2639@gmail.com>
2020-03-23 11:04:46 -0500

    added express middleware to handle requests, sends back static public files

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+let app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from the web server side...");
+// });
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.listen(3000);
